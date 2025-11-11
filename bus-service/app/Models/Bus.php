41b78527e52bf68f216e97f8f19ecc88566d1bd9
varelsaurus <mhdvarels@gmail.com>
2025-11-11 20:58:34 +0700

    Configurasi Endpoint

--- bus-service/app/Models/Bus.php
+++ bus-service/app/Models/Bus.php
@@ -2,8 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Bus extends Model {
+class Bus extends Model
+{
+    use HasFactory;
+
     protected $fillable = ['code', 'route_id', 'capacity', 'lat', 'lng'];
 }
